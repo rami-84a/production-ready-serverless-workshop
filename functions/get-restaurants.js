a73b6cce37142023258fd04a28adacb672855e22
Rami Ammari <rami.ammari@coliquio.de>
2018-12-01 17:22:50 +0200

    X-Ray & capture and forward correlation ID via HTTP

--- functions/get-restaurants.js
+++ functions/get-restaurants.js
@@ -1,6 +1,10 @@
-const AWS = require('aws-sdk')
+const AWSXRay = require('aws-xray-sdk-core')
+const AWS = process.env.LAMBDA_RUNTIME_DIR
+  ? AWSXRay.captureAWS(require('aws-sdk'))
+  : require('aws-sdk')
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 const wrap = require('../lib/wrapper');
+const Log = require('../lib/log');
 
 const defaultResults = process.env.defaultResults || 8
 const tableName = process.env.restaurants_table
@@ -16,7 +20,8 @@ const getRestaurants = async (count) => {
 }
 
 module.exports.handler = wrap(async (event, context) => {
-  const restaurants = await getRestaurants(defaultResults)
+  const restaurants = await getRestaurants(defaultResults);
+  Log.debug(`fetched ${restaurants.length} restaurants`);
   const response = {
     statusCode: 200,
     body: JSON.stringify(restaurants)
--- middleware/sample-logging.js
+++ middleware/sample-logging.js
@@ -1,13 +1,21 @@
-const Log = require('../lib/log')
+const Log = require('../lib/log');
+const CorrelationIds = require('../lib/correlation-ids');
 
 // config should be { sampleRate: double } where sampleRate is between 0.0-1.0
 module.exports = (config) => {
-  const sampleRate = config ? config.sampleRate || 0.01 : 0.01 // defaults to 1%
+  const sampleRate = config ? config.sampleRate || 0.01 : 0.01; // defaults to 1%
   let rollback = undefined
 
   const isDebugEnabled = () => {
-    return sampleRate && Math.random() <= sampleRate
-  };
+    const context = CorrelationIds.get()
+    if (context['debug-log-enabled'] === 'true') {
+      return true
+    } else if (context['debug-log-enabled'] === 'false') {
+      return false
+    } else {
+      return sampleRate && Math.random() <= sampleRate
+    }
+  }
 
   return {
     before: (handler, next) => {
